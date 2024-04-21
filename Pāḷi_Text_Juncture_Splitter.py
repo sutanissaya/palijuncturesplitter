1da143c2a18ebbab335c0e14e26aa8aaffa01688
Sutanissaya <167442356+sutanissaya@users.noreply.github.com>
2024-04-21 22:57:32 +0700

    Update Pāḷi_Text_Juncture_Splitter.py

--- Pāḷi_Text_Juncture_Splitter.py
+++ Pāḷi_Text_Juncture_Splitter.py
@@ -2670,8 +2670,15 @@ def animation_demo() -> None:
                 v_or_w = first_letters
  
     # (Optional) Saṃyoga Pauses; Breaks for short open syllables at the end of a phrase/sentence; Insert juncture sign after the short vowels a, i, and u if they preceed a line break, a double space (tab/indentations), a comma, a semi-colon, or a colon:
+    sepa_lbreak = v_or_w.replace("  \n", sepa+sepa+"  \n")
+    sepa_dspace = sepa_lbreak.replace("  ", sepa+sepa+"  ")
+    sepa_comma = sepa_dspace.replace(",", sepa+sepa+",")
+    sepa_period = sepa_comma.replace(".", sepa+sepa+".")
+    sepa_scolon = sepa_period.replace(";", sepa+sepa+";")
+    sepa_colon = sepa_scolon.replace(":", sepa+sepa+":")
+ 
     # lowercase
-    undo_a_sepa_lbreak = v_or_w.replace("a"+sepa+sepa+"  \n", "a"+"  \n")
+    undo_a_sepa_lbreak = sepa_colon.replace("a"+sepa+sepa+"  \n", "a"+"  \n")
     undo_i_sepa_lbreak = undo_a_sepa_lbreak.replace("i"+sepa+sepa+"  \n", "i"+"  \n")
     undo_u_sepa_lbreak = undo_i_sepa_lbreak.replace("u"+sepa+sepa+"  \n", "u"+"  \n")
 
@@ -2731,7 +2738,7 @@ def animation_demo() -> None:
     if samyoga_pauses_check:
         sepa_colon_OR_sepa_sepa_colon = sepa_colon
     else:
-        sepa_colon_OR_sepa_sepa_colon = v_or_w
+        sepa_colon_OR_sepa_sepa_colon = sepa_colon
         # Fix the position of preposition pauses and junctions signs
         sepa_sepa_comma = sepa_colon_OR_sepa_sepa_colon.replace(sepa+sepa+",",","+sepa+sepa)
         sepa_sepa_period = sepa_sepa_comma.replace(sepa+sepa+".","."+sepa+sepa)
