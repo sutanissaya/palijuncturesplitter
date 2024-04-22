0f7c8adfa4e9a4aec0fcbfeeb6eb2b1bbfef8951
Sutanissaya <167442356+sutanissaya@users.noreply.github.com>
2024-04-22 15:11:55 +0700

    Update Pāḷi_Text_Juncture_Splitter.py

--- Pāḷi_Text_Juncture_Splitter.py
+++ Pāḷi_Text_Juncture_Splitter.py
@@ -2510,11 +2510,11 @@ def animation_demo() -> None:
             if ṃṁ_select == "ṃ, ṁ → m":
                 ṃ_to_ng = Ṅ_TO_NG.replace("ṃ", "m")
                 ṁ_to_ng = ṃ_to_ng.replace("ṁ", "m")
-                    if uppercase_check:
-                        Ṃ_TO_NG = ṁ_to_ng.replace("Ṃ", "M")
-                        Ṁ_TO_NG = Ṃ_TO_NG.replace("Ṁ", "M") 
-                    else:
-                        Ṁ_TO_NG = Ṅ_TO_NG
+                if uppercase_check:
+                    Ṃ_TO_NG = ṁ_to_ng.replace("Ṃ", "M")
+                    Ṁ_TO_NG = Ṃ_TO_NG.replace("Ṁ", "M") 
+                else:
+                    Ṁ_TO_NG = Ṅ_TO_NG
     
     # Saṃyoga Consonants
     if samyoga_consonants_check:
