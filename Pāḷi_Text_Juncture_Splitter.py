4952b079a277f26181eee1ad9f40dd4631ac4174
Sutanissaya <167442356+sutanissaya@users.noreply.github.com>
2024-04-22 13:37:45 +0700

    Update Pāḷi_Text_Juncture_Splitter.py

--- Pāḷi_Text_Juncture_Splitter.py
+++ Pāḷi_Text_Juncture_Splitter.py
@@ -2612,22 +2612,21 @@ def animation_demo() -> None:
 
     # Extra juncture sign after a double space "  ", a comma ",", a period ".", semi-colon";", colon ":", or a line break / enter (char(10) or \n)
     sepa_lbreak = v_or_w.replace("  \n", sepa+"  \n")
-    sepa_comma = sepa_lbreak.replace(",", ","+sepa)
-    fix_sepa_comma = sepa_comma.replace(","+sepa+sepa+sepa, ","+sepa+sepa)
-    dspace_sepa = fix_sepa_comma.replace("  ", sepa+"  ")
-    fix_dspace_sepa = dspace_sepa.replace(sepa+sepa+sepa+"  ", sepa+sepa+"  ")
-    fix_xsepa = fix_dspace_sepa.replace(sepa+sepa+sepa+sepa+"  ", sepa+sepa+"  ")
+    comma_sepa = sepa_lbreak.replace(",", ","+sepa)
+    fix_comma_sepa = comma_sepa.replace(","+sepa+sepa+sepa, ","+sepa+sepa)
+    period_sepa = fix_comma_sepa.replace(".", sepa+".")
+    fix_period_sepa = period_sepa.replace(sepa+sepa+sepa+".", sepa+sepa+".")
 
  
     # Show Unsplit Line by Line
     if show_unsplit:
         input_lines = insert_text.split('\n')
-        output_lines = fix_xsepa.split('\n')
+        output_lines = fix_period_sepa.split('\n')
         for i in range(len(input_lines)):
             unsplit_OR_split = input_lines[i]+'\n'+'\n'+output_lines[i]+'\n'
             st.write(unsplit_OR_split)
     else:
-        unsplit_OR_split = fix_xsepa
+        unsplit_OR_split = fix_period_sepa
         st.write(unsplit_OR_split)
       
 animation_demo()
